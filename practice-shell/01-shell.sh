d412c79f791e350f039efcedf5060e75d4289b08
sari2016 <sarithagovindaraju@gmail.com>
2025-01-20 22:21:25 -0600

    practice

---
+++ practice-shell/01-shell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ echo "hello!!!"
+
+ NUMBER=$1
+ NUMBER=$2
+
+ if [ $NUMBER -gt 100 ]
+ then
+ echo " The given number is greater than 100"
+ else
+ echo " The given number is less then 100"
+ fi
+ 
