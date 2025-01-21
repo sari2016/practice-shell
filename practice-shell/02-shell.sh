e5e0cf6dbd7da10acadae91eed167985b0168e4a
sari2016 <sarithagovindaraju@gmail.com>
2025-01-21 01:05:29 -0600

    practice

--- practice-shell/02-shell.sh
+++ practice-shell/02-shell.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
 VALIDATE(){
-    if ( $1 -ne 0 )
+    if [ $1 -ne 0 ]
     then
     echo "$2 .. FAILURE"
     else
     echo "$2 .. SUCCESS"
     fi 
 }
-if ( $USERID -ne 0 )
+if [ $USERID -ne 0 ]
 then
 echo " ERROR: you must have sudo privilages to execute this script"
 exit 1
